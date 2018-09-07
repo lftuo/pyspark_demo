f8966a421e8a2ed9e6b43abb9be67bff6113fea7
lftuo <909709223@qq.com>
2018-09-07 17:23:20 +0800

    上传最新代码

--- ModelSourceTableMonitorPy27.py
+++ ModelSourceTableMonitorPy27.py
@@ -13,9 +13,7 @@ import datetime
 import multiprocessing
 import sys
 import logging
-#from impala import dbapi
-#from impala.dbapi import connect
-#from impala.util import as_pandas
+import json
 
 
 def get_logger(name, level=logging.INFO):
@@ -73,17 +71,19 @@ def load_mysql_data(host_name, port, db_name, username, password):
     return jdbcDF
 
 
-#def query_result_on_hadoop(query):
-#    try:
-#        conn = dbapi.connect(host='168.9.65.71', port='21050')
-#        cursor = conn.cursor(user='crmpicturewr')
-#        cursor.executor(query)
-#        return as_pandas(cursor)
-#    except Exception as e:
-#        logger.error("conn imapa failed ! error msg is [{0}]".format(e))
-#    finally:
-#        cursor.close()
-#        conn.close()
+def query_result_on_hadoop(query):
+    from impala import dbapi
+    from impala.util import as_pandas
+    try:
+        conn = dbapi.connect(host='localhost', port='21050')
+        cursor = conn.cursor(user='crmpicturewr')
+        cursor.execute(query)
+        return as_pandas(cursor)
+    except Exception as e:
+        logger.error("conn imapa failed ! error msg is [{0}]".format(e))
+    finally:
+        cursor.close()
+        conn.close()
 
 
 def check_data(tab):
@@ -94,16 +94,7 @@ def check_data(tab):
     :return:
     '''
 
-    logger = get_logger("check_data")
-    conn_info = tab.conn_info
-    dt = conn_info["dt"]
-    host_name = conn_info["host_name"]
-    port = conn_info["port"]
-    db_name = conn_info["db_name"]
-    username = conn_info["username"]
-    password = conn_info["password"]
-    logger.info("conn params is [{0},{1},{2},{3},{4},{5}]".format(dt, host_name, port, db_name, username, password))
-
+    dt = tab.dt
     model_table_name = tab.model_tab_name
     model_tab_type = tab.model_tab_type
     model_pri_key = tab.model_pri_key
@@ -113,20 +104,16 @@ def check_data(tab):
     source_pri_key = tab.source_pri_key
     source_tab_par = tab.source_tab_par
     status = tab.status
-    logger.info("model params is [{0},{1},{2},{3},{4},{5},{6},{7},{8}]".format(model_table_name, model_tab_type, model_pri_key, model_tab_par, source_pri_tab, source_tab_type, source_pri_key, source_tab_par, status))
+    logger.info("model params is [{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}]".format(dt,model_table_name, model_tab_type, model_pri_key, model_tab_par, source_pri_tab, source_tab_type, source_pri_key, source_tab_par, status))
 
     # 如果status==1，则进行数据监控
     if status == 1:
-        logger.info("---------------------------------1")
-
-        logger.info("---------------------------------2")
         # 如果当前表单为月表，则根据dt获取上月时间数据：
         # 如：dt=20180824 则时间日期为201807
         if model_tab_type == 2:
             model_real_dt = last_month(dt)
         else:
             model_real_dt = dt
-        logger.info("---------------------------------3")
         # 监控表单数据总量查询SQL
         cnt_sql = """
             select 
@@ -137,32 +124,27 @@ def check_data(tab):
                 {} = '{}'
         """.format(model_table_name, model_tab_par, model_real_dt)
         logger.info("cnt_sql is [{0}]".format(cnt_sql))
-        cnt = spark.sql(cnt_sql).rdd.collect()[0].cnt
+        # cnt = spark.sql(cnt_sql).rdd.collect()[0].cnt
+        try:
+            cnt = query_result_on_hadoop(cnt_sql).cnt[0]
+        except Exception as e:
+            logger.error("query model table error [{0}]".format(e))
+            cnt = -1
+            rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':int(cnt), 'null_rate':-1}]
+            return rst_df
+
         logger.info("cnt is [{0}]".format(cnt))
-
-
-        logger.info("---------------------------------4")
         if cnt == 0:
-            logger.info("---------------------------------5")
             # 如果当前监控表单数据总量为0，则空值比例为-1，表示当前表数据未到，spark SQL插入查询结果
             rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':0, 'null_rate':-1}]
             logger.info("rst_df is [{0}]".format(rst_df))
-            spark.createDataFrame(rst_df).write.\
-                mode("append").\
-                format("jdbc").\
-                option("url", "jdbc:mysql://%s:%s/%s" % (host_name, port, db_name)). \
-                option("dbtable", "model_source_monitor_rslt"). \
-                option("user", "%s" % (username)). \
-                option("password", "%s" % (password)).\
-                save()
+            return rst_df
         elif cnt > 0:
-            logger.info("---------------------------------6")
             # 如果当前监控表单的数据总量>0，则计算主表数据总量
             if source_tab_type == 2:
                 source_real_dt = last_month(dt)
             else:
                 source_real_dt = dt
-            logger.info("---------------------------------7")
             pri_cnt_sql = """
                 select 
                     count(1) as cnt 
@@ -172,29 +154,24 @@ def check_data(tab):
                     {} = '{}'
             """.format(source_pri_tab, source_tab_par, source_real_dt)
             logger.info("pri_cnt_sql is [{0}]".format(pri_cnt_sql))
-            pri_cnt = spark.sql(pri_cnt_sql).rdd.collect()[0].cnt
-            logger.info("---------------------------------8")
+            # pri_cnt = spark.sql(pri_cnt_sql).rdd.collect()[0].cnt
+            try:
+                pri_cnt = query_result_on_hadoop(pri_cnt_sql).cnt[0]
+            except Exception as e:
+                logger.error("query source table error [{0}]".format(e))
+                rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':int(cnt), 'null_rate':-100}]
+                return rst_df
+
             if pri_cnt == 0:
-                logger.info("---------------------------------9")
                 # 如果主表的数据总量为0，则空值比例为-100，表示主表数据未到
-                rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':cnt, 'null_rate':-100}]
+                rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':int(cnt), 'null_rate':-100}]
                 logger.info("rst_df is [{0}]".format(rst_df))
-                logger.info("---------------------------------10")
-                spark.createDataFrame(rst_df).write. \
-                    mode("append"). \
-                    format("jdbc"). \
-                    option("url", "jdbc:mysql://%s:%s/%s" % (host_name, port, db_name)). \
-                    option("dbtable", "model_source_monitor_rslt"). \
-                    option("user", "%s" % (username)). \
-                    option("password", "%s" % (password)). \
-                    save()
-                logger.info("---------------------------------11")
+                return rst_df
             elif pri_cnt > 0:
-                logger.info("---------------------------------12")
                 # 关联查询计算空值比例
                 join_sql = """
                     select
-                        count(1)
+                        count(1) as cnt
                     from 
                     (
                         select
@@ -212,26 +189,23 @@ def check_data(tab):
                             {}
                         where
                             {} = '{}'
-                    ) t2 on t1.{} = t2.{} where t1.{} is null
+                    ) t2 on t1.{} = t2.{} where t2.{} is null
                 """.format(model_pri_key, model_tab_par, model_table_name, model_tab_par, model_real_dt, source_pri_key, source_tab_par,
-                           source_pri_tab, source_tab_par, source_real_dt, model_tab_par, source_tab_par, model_pri_key)
+                           source_pri_tab, source_tab_par, source_real_dt, model_pri_key, source_pri_key, model_pri_key)
                 logger.info("join_sql is [{0}]".format(join_sql))
-                null_cnt = spark.sql(join_sql).rdd.collect()[0].cnt
+
+                try:
+                    null_cnt = query_result_on_hadoop(join_sql).cnt[0]
+                except Exception as e:
+                    null_cnt = -1
+                    logger.error("query null cnt error [{0}]".format(e))
+                    rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':cnt, 'null_rate':null_cnt}]
+                    return rst_df
                 logger.info("null cnt is [{}]".format(null_cnt))
-                logger.info("---------------------------------13")
-                nullRate = format(float(null_cnt)/float(pri_cnt), '.9f')
-                rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':cnt, 'null_rate':nullRate}]
+                nullRate = float(format(float(null_cnt)/float(pri_cnt), '.9f'))
+                rst_df = [{'tab_name':model_table_name, 'dt':dt, 'last_count':int(cnt), 'null_rate':nullRate}]
                 logger.info("rst_df is [{0}]".format(rst_df))
-                logger.info("---------------------------------14")
-                spark.createDataFrame(rst_df).write. \
-                    mode("append"). \
-                    format("jdbc"). \
-                    option("url", "jdbc:mysql://%s:%s/%s" % (host_name, port, db_name)). \
-                    option("dbtable", "model_source_monitor_rslt"). \
-                    option("user", "%s" % (username)). \
-                    option("password", "%s" % (password)). \
-                    save()
-                logger.info("---------------------------------15")
+                return rst_df
 
 
 if __name__ == '__main__':
@@ -256,11 +230,9 @@ if __name__ == '__main__':
     thread_num = sys.argv[7]
     logger.error("params is dt:{}, host_name:{}, port:{}, db_name:{}, username:{}, password:{}, thread_num:{}".format(dt, host_name, port, db_name, username, password, thread_num))
 
-    conn_info = {'dt' : dt, 'host_name' : host_name, 'port' : port, 'db_name' : db_name, 'username' : username, 'password' : password}
     # 查询MySQL配置表单数据
     df = load_mysql_data(host_name, port, db_name, username, password)
-
-    tabs = df.rdd.map(lambda x: Row(conn_info = conn_info,
+    tabs = df.rdd.map(lambda x: Row(dt = str(dt),
                                     model_tab_name = str(x[0]),
                                     model_tab_type = x[1],
                                     model_pri_key = str(x[2]),
@@ -273,10 +245,33 @@ if __name__ == '__main__':
 
     # 定义同时至多起几个线程
     pool = multiprocessing.Pool(int(thread_num))
+    results = []
     for tab in tabs:
-        pool.apply_async(check_data, args=(tab,))
-
+        try:
+            rst_df = pool.apply_async(check_data, args=(tab,))
+            # 此处不要直接进行插入操作，影响并发效率，先将变量存储
+            results.append(rst_df)
+        except Exception as e:
+            logger.error("single thread error , tab is [{0}], error msg is [{1}]".format(tab, e))
     # 用来阻止多余的进程涌入进程池 Pool 造成进程阻塞。
     pool.close()
     # 方法实现进程间的同步，等待所有进程退出。
     pool.join()
+
+    # 开始查询结果的插入
+    logger.info("start insert result into mysql ....")
+    for rslt in results:
+        # spark插入查询结果
+        try:
+            logger.info("rslt is [{0}]".format(rslt.get()))
+            spark.createDataFrame(rslt.get()).write. \
+                mode("append"). \
+                format("jdbc"). \
+                option("url", "jdbc:mysql://%s:%s/%s" % (host_name, port, db_name)). \
+                option("dbtable", "model_source_monitor_rslt"). \
+                option("user", "%s" % (username)). \
+                option("password", "%s" % (password)). \
+                save()
+        except Exception as e:
+            logger.error("insert into mysql error [{0}]".format(e))
+    logger.info("end insert result into mysql .")
